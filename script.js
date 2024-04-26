ef138e33fb12f43e027abe5ff82d5d3e5ca01874
imran malick <163141139+imwebdesigner14@users.noreply.github.com>
2024-04-26 08:16:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// Get the button element
+var button = document.getElementById("togglebox");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // Function to run when the button is clicked
+  alert("Button clicked!");
+});
+ 
